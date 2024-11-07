3831e90d3462fb13045e4c35938ee21f76da91cd
Emily Medeiros <161541242+EmilyMedeiross@users.noreply.github.com>
2024-11-07 09:48:03 -0300

    Add files via upload

---
+++ Q4/models/estudante.py
@@ -0,0 +1,5 @@
+class Estudante:
+    def __init__(self, nome: str, matricula: int):
+        self.nome = nome
+        self.matricula = matricula
+    
---
+++ Q4/models/livro.py
@@ -0,0 +1,5 @@
+class Livro:
+    def __init__(self, titulo: str, autor: str):
+        self.titulo = titulo
+        self.autor = autor
+ 
